649024f0a1111cd37ebd5a7be41acc16ac9477f4
sebastianriosanchez68@gmail.com <sebastianriosanchez68@gmail.com>
2019-03-23 11:31:11 -0400

    Api + Azure

---
+++ productoBackend/api2/Models/DataContex.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+namespace api2.Models
+{
+    public class DataContex:DbContext
+    {
+        public DataContex():base("DefaultConnection")
+        {
+
+        }
+
+        public System.Data.Entity.DbSet<api2.Models.Product> Products { get; set; }
+    }
+}
---
+++ productoBackend/api2/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿namespace api2.Models
+{
+    using System.ComponentModel.DataAnnotations;
+
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre del producto")]
+        public string Nombre { get; set; }
+        public double Precio { get; set; }
+    }
+}
